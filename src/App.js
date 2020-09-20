85d0899e7de4ead4d09af7ba09f0d7df8988e3a1
Charles <codelyfewithcharlie@gmail.com>
2020-09-20 10:24:06 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import Scoreboard from './Components/Scoreboard/Scoreboard';
 import styles from './App.module.scss';
 import ButtonContainer from './Components/ButtonContainer/ButtonContainer';
+import Button from './Components/Buttons/Buttons.js';
 import { GameLogic } from './playGame';
 function App() {
   const [score, setScore] = useState(0);
-  const [house, setHouse] = useState('');
+  const [house, setHouse] = useState(0);
   const [myChoice, setMyChoice] = useState('');
   const [gameOver, setGameOver] = useState(false);
 
@@ -21,16 +22,29 @@ function App() {
     setGameOver(true);
   };
 
+  const getHouseChoice = (house) => {
+    if (house === 1) {
+      return 'rock';
+    } else if (house === 2) {
+      return 'paper';
+    } else if (house === 3) {
+      return 'scissors';
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Scoreboard score={score} />
       {gameOver ? (
         <div>
+          <Button logo={myChoice} />
           <button onClick={() => setGameOver(false)}>reset</button>
+          <Button logo={getHouseChoice(house)} />
         </div>
       ) : (
         <ButtonContainer play={playGame} />
       )}
+      <button>Rules</button>
     </div>
   );
 }
